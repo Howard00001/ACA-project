18b4de513c656df946b183c7c61b310ae3032975
superhoan <hoan704@gmail.com>
2022-09-12 23:24:02 +0800

    front

--- aca-simp/func5/getAllNames.m
+++ aca-simp/func5/getAllNames.m
@@ -1,7 +1,11 @@
-function names = getAllNames(paths)
+function names = getAllNames(paths, cattype)
     tmp = split(paths,'/');
     files = tmp(:,:,end);
     tmp = split(files,'-');
-    names = strcat(tmp(:,:,1), '-',tmp(:,:,2));
+    if cattype
+        names = strcat(tmp(:,:,1), '-',tmp(:,:,2));
+    else
+        names = tmp(:,:,1);
+    end
     names = unique(names);
 end
